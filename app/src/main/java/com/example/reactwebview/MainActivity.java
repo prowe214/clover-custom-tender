d3a7fc031c46dc6be80bf10bef1df1f120f99e1b
Paul Rowe <prowe214@gmail.com>
2019-11-12 11:58:34 -0700

    init

---
+++ app/src/main/java/com/example/reactwebview/MainActivity.java
@@ -0,0 +1,120 @@
+//package com.example.reactwebview;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.content.Context;
+////import android.content.Intent;
+//import android.os.AsyncTask;
+//import android.os.Bundle;
+//import android.util.Log;
+//import android.view.View;
+//import android.webkit.JavascriptInterface;
+//import android.webkit.WebView;
+//import android.webkit.WebSettings;
+//
+//import com.clover.sdk.util.CloverAccount;
+//import com.clover.sdk.v1.tender.TenderConnector;
+//
+//
+//class WebAppInterface {
+//    Context mContext;
+//
+//    public static final String TAG = "JS";
+//    long amount;
+//    Currency currency;
+//    long taxAmount;
+//    ArrayList taxableAmounts;
+//    ServiceChargeAmount serviceCharge;
+//
+//    String orderId;
+//    String employeeId;
+//    String merchantId;
+//
+//    Tender tender;
+//
+//    // Customer Facing specific fields
+//    long tipAmount;
+//
+//    WebAppInterface(Context c) {
+//        mContext = c;
+//    }
+//
+//    /**
+//     * doSale
+//     *
+//     * STUB handling submission to Clover SDK.
+//     * This will pass a value to Clover upon a successful transaction.
+//     * */
+//    @JavascriptInterface
+//    public String doSale(String amount) {
+//        Log.w(TAG, "HIT");
+////        Intent i = new Intent(Intents.ACTION_SECURE_PAY);
+////        i.putExtra(Intents.EXTRA_AMOUNT, Long.valueOf(amount));
+////        startActivityForResult(i, WEB_POS_SALE_REQUEST_CODE);
+//        return amount;
+//    }
+//}
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        // Necessary for Customer Facing user experiences
+//        setSystemUiVisibility();
+//
+//        WebView myWebView = findViewById(R.id.webview);
+//        myWebView.loadUrl("https://awesome-stonebraker-38b0e2.netlify.com/");
+//        myWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
+//        WebSettings webSettings = myWebView.getSettings();
+//        webSettings.setJavaScriptEnabled(true);
+//    }
+//
+//    private static void createTenderType(final Context context) {
+//        new AsyncTask<Void, Void, Exception>() {
+//
+//            private TenderConnector tenderConnector;
+//
+//            @Override
+//            protected void onPreExecute() {
+//                super.onPreExecute();
+//                tenderConnector = new TenderConnector(context, CloverAccount.getAccount(context), null);
+//                tenderConnector.connect();
+//            }
+//
+//            @Override
+//            protected Exception doInBackground(Void... params) {
+//                try {
+//                    tenderConnector.checkAndCreateTender("CareCreditPayment", context.getPackageName(), true, false); // initialization
+//                } catch (Exception exception) {
+//                    Log.e("DIED", exception.getMessage(), exception.getCause());
+//                    return exception;
+//                }
+//                return null;
+//            }
+//
+//            @Override
+//            protected void onPostExecute(Exception exception) {
+//                tenderConnector.disconnect();
+//                tenderConnector = null;
+//            }
+//        }.execute();
+//    }
+//
+//    public void setSystemUiVisibility() {
+//        getWindow().getDecorView().setSystemUiVisibility(
+//                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                        | View.SYSTEM_UI_FLAG_LOW_PROFILE
+//                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+//                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+//    }
+//
+//
+//}
+//
+//

53d9c06bc0d3b00ab7d8401b3380308bfa207919
Paul Rowe <prowe214@gmail.com>
2019-11-14 11:25:38 -0700

    trying to fix default activity

--- app/src/main/java/com/example/reactwebview/MainActivity.java
+++ app/src/main/java/com/example/reactwebview/MainActivity.java
@@ -1,120 +1,147 @@
-//package com.example.reactwebview;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import android.content.Context;
-////import android.content.Intent;
-//import android.os.AsyncTask;
-//import android.os.Bundle;
-//import android.util.Log;
-//import android.view.View;
-//import android.webkit.JavascriptInterface;
-//import android.webkit.WebView;
+package com.example.reactwebview;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.Intent;
+import android.os.Bundle;
 //import android.webkit.WebSettings;
-//
-//import com.clover.sdk.util.CloverAccount;
-//import com.clover.sdk.v1.tender.TenderConnector;
-//
-//
-//class WebAppInterface {
-//    Context mContext;
-//
-//    public static final String TAG = "JS";
-//    long amount;
-//    Currency currency;
-//    long taxAmount;
-//    ArrayList taxableAmounts;
-//    ServiceChargeAmount serviceCharge;
-//
-//    String orderId;
-//    String employeeId;
-//    String merchantId;
-//
-//    Tender tender;
-//
-//    // Customer Facing specific fields
-//    long tipAmount;
-//
-//    WebAppInterface(Context c) {
-//        mContext = c;
-//    }
-//
-//    /**
-//     * doSale
-//     *
-//     * STUB handling submission to Clover SDK.
-//     * This will pass a value to Clover upon a successful transaction.
-//     * */
-//    @JavascriptInterface
-//    public String doSale(String amount) {
-//        Log.w(TAG, "HIT");
-////        Intent i = new Intent(Intents.ACTION_SECURE_PAY);
-////        i.putExtra(Intents.EXTRA_AMOUNT, Long.valueOf(amount));
-////        startActivityForResult(i, WEB_POS_SALE_REQUEST_CODE);
-//        return amount;
-//    }
-//}
-//
-//public class MainActivity extends AppCompatActivity {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        // Necessary for Customer Facing user experiences
-//        setSystemUiVisibility();
-//
-//        WebView myWebView = findViewById(R.id.webview);
+//import android.webkit.WebView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.clover.sdk.util.CloverAccount;
+import com.clover.sdk.v1.ResultStatus;
+import com.clover.sdk.v1.tender.Tender;
+import com.clover.sdk.v1.tender.TenderConnector;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private static final int REQUEST_ACCOUNT = 1;
+
+    private TenderConnector tenderConnector;
+    private Account account;
+
+    private TextView resultText;
+//    WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        resultText = (TextView) findViewById(R.id.result);
+//        myWebView = findViewById(R.id.webview);
 //        myWebView.loadUrl("https://awesome-stonebraker-38b0e2.netlify.com/");
 //        myWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
 //        WebSettings webSettings = myWebView.getSettings();
 //        webSettings.setJavaScriptEnabled(true);
-//    }
-//
-//    private static void createTenderType(final Context context) {
-//        new AsyncTask<Void, Void, Exception>() {
-//
-//            private TenderConnector tenderConnector;
-//
-//            @Override
-//            protected void onPreExecute() {
-//                super.onPreExecute();
-//                tenderConnector = new TenderConnector(context, CloverAccount.getAccount(context), null);
-//                tenderConnector.connect();
-//            }
-//
-//            @Override
-//            protected Exception doInBackground(Void... params) {
-//                try {
-//                    tenderConnector.checkAndCreateTender("CareCreditPayment", context.getPackageName(), true, false); // initialization
-//                } catch (Exception exception) {
-//                    Log.e("DIED", exception.getMessage(), exception.getCause());
-//                    return exception;
-//                }
-//                return null;
-//            }
-//
-//            @Override
-//            protected void onPostExecute(Exception exception) {
-//                tenderConnector.disconnect();
-//                tenderConnector = null;
-//            }
-//        }.execute();
-//    }
-//
-//    public void setSystemUiVisibility() {
-//        getWindow().getDecorView().setSystemUiVisibility(
-//                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-//                        | View.SYSTEM_UI_FLAG_LOW_PROFILE
-//                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-//                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-//    }
-//
-//
-//}
-//
-//
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_ACCOUNT && resultCode == RESULT_OK) {
+            String name = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+            String type = data.getStringExtra(AccountManager.KEY_ACCOUNT_TYPE);
+
+            account = new Account(name, type);
+        }
+    }
+
+    private void startAccountChooser() {
+        Intent intent = AccountManager.newChooseAccountIntent(null, null, new String[]{CloverAccount.CLOVER_ACCOUNT_TYPE}, false, null, null, null, null);
+        startActivityForResult(intent, REQUEST_ACCOUNT);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (account != null) {
+            connect();
+            createTender();
+        } else {
+            startAccountChooser();
+        }
+    }
+
+
+    @Override
+    protected void onPause() {
+        disconnect();
+        super.onPause();
+    }
+
+    private void connect() {
+        disconnect();
+        if (account != null) {
+            tenderConnector = new TenderConnector(this, account, null);
+            tenderConnector.connect();
+        }
+    }
+
+    private void disconnect() {
+        if (tenderConnector != null) {
+            tenderConnector.disconnect();
+            tenderConnector = null;
+        }
+    }
+
+    private void getTenders() {
+        tenderConnector.getTenders(new TenderConnector.TenderCallback<List<Tender>>() {
+            @Override
+            public void onServiceSuccess(List<Tender> result, ResultStatus status) {
+                super.onServiceSuccess(result, status);
+                String text = "Tenders:\n";
+                for (Tender t : result) {
+                    text += "  " + t.getId() + " , " + t.getLabel() + " , " + t.getLabelKey() + " , " + t.getEnabled() + " , " + t.getOpensCashDrawer() + "\n";
+                }
+                resultText.setText(text);
+            }
+
+            @Override
+            public void onServiceFailure(ResultStatus status) {
+                super.onServiceFailure(status);
+                resultText.setText(status.getStatusMessage());
+            }
+
+            @Override
+            public void onServiceConnectionFailure() {
+                super.onServiceConnectionFailure();
+                resultText.setText("Service Connection Failure");
+            }
+        });
+    }
+
+    private void createTender() {
+        final String tenderName = "Clover Example Tender";
+        final String packageName = getPackageName();
+
+        tenderConnector.checkAndCreateTender(tenderName, packageName, true, false, new TenderConnector.TenderCallback<Tender>() {
+            @Override
+            public void onServiceSuccess(Tender result, ResultStatus status) {
+                super.onServiceSuccess(result, status);
+                String text = "Custom Tender:\n";
+                text += "  " + result.getId() + " , " + result.getLabel() + " , " + result.getLabelKey() + " , " + result.getEnabled() + " , " + result.getOpensCashDrawer() + "\n";
+                resultText.setText(text);
+            }
+
+            @Override
+            public void onServiceFailure(ResultStatus status) {
+                super.onServiceFailure(status);
+                resultText.setText(status.getStatusMessage());
+            }
+
+            @Override
+            public void onServiceConnectionFailure() {
+                super.onServiceConnectionFailure();
+                resultText.setText("Service Connection Failure");
+            }
+        });
+    }
+
+}
